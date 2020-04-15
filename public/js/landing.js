2179bfd9e790d7245a709e7acb061d8214eca41a
David Pendley <dpendley401@gmail.com>
2020-04-15 15:28:31 -0700

    modified landing page to read background image and cleaned up errors

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $(".carousel.carousel-slider").carousel({
-    fullWidth: true
-  });
-  instance.next();
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
